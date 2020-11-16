5c81fc43d6222d7d424cc4fe3ce2a12aca72502f
Eashwar Ranganathan <eashwar@eashwar.com>
2020-11-16 01:11:17 -0800

    Refactor build script and add more targets

---
+++ builder.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+set -euo pipefail
+
+echo "Cleaning and updating git repo"
+git clean -fxq
+git pull --rebase
+
+echo "Applying patches"
+git apply --ignore-whitespace /patches/*.patch
+
+make -C src $TARGETS
+
+echo
+echo "Build finished successfully!"
+
+echo
+echo "Copying build artifacts:"
+for bin in $TARGETS; do
+	mkdir -p "$(dirname "/out/$bin")"
+	cp -v "src/$bin" "/out/$bin"
+done
+
+echo
+echo "Building tarball:"
+tar czvf /out/ipxe-bundle.tar.gz -C src $TARGETS

b4712c5900576fc9e93c4359b7fb4dbeed7b02ea
Eashwar Ranganathan <eashwar@eashwar.com>
2020-11-16 02:23:31 -0800

    Pass targets in as arguments instead of using an env var

--- builder.sh
+++ builder.sh
@@ -1,6 +1,12 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
+TARGETS=()
+while [[ $# -gt 0 ]]; do
+	TARGETS+=("$1")
+	shift
+done
+
 echo "Cleaning and updating git repo"
 git clean -fxq
 git pull --rebase
@@ -8,18 +14,18 @@ git pull --rebase
 echo "Applying patches"
 git apply --ignore-whitespace /patches/*.patch
 
-make -C src $TARGETS
+make -C src "${TARGETS[@]}"
 
 echo
 echo "Build finished successfully!"
 
 echo
 echo "Copying build artifacts:"
-for bin in $TARGETS; do
+for bin in "${TARGETS[@]}"; do
 	mkdir -p "$(dirname "/out/$bin")"
 	cp -v "src/$bin" "/out/$bin"
 done
 
 echo
 echo "Building tarball:"
-tar czvf /out/ipxe-bundle.tar.gz -C src $TARGETS
+tar czvf /out/ipxe-bundle.tar.gz -C src "${TARGETS[@]}"
